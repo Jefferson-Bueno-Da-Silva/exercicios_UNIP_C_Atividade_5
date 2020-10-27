c69c3f64fa1d3573e044140e52265f29985e6c2a
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2020-10-27 18:32:42 -0300

    ex007

--- ex007.c
+++ ex007.c
@@ -1,4 +1,12 @@
-/**/
+/*
+
+7. Crie um programa que leia o preço de compra e o preço de venda de 100 mercadorias (utilize
+vetores). Ao final, o programa deverá imprimir quantas mercadorias proporcionam:
+○ lucro < 10%
+○ 10% <= lucro <= 20%
+○ lucro > 20%
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
