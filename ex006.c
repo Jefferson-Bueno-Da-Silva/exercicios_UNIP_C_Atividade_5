21da7992b6e3b45e15395be53c7d07cddc9e87a1
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2020-10-27 18:28:59 -0300

    ex006

--- ex006.c
+++ ex006.c
@@ -9,7 +9,31 @@
 
 int main()
 {
+    //VARIAVEIS
+    float salario[20], salarioNovo[20];
+    int i;
     system("cls");
+
+    //ENTRADA
+    printf("Digite o valor do salario dos funcionarios para o reajuste: \n");
+
+    for (i = 0; i < 20 ; i++)
+    {
+        printf("Digite o Salario do %i funcionario: \n", i + 1);
+        scanf("%f", &salario[i]);
+        salarioNovo[i] = (salario[i] * 8) / 100; 
+    }
     
+    //SAIDA
+    system("cls");
+    printf("Funcionario\tSalario |\t| Salario novo\n");
+    for(i = 0; i < 20; i++){
+        printf("-----------------------------------------\n");
+        printf("%i\t\t%.2f |\t| %.2f\n", i + 1, salario[i], salario[i] + salarioNovo[i]);
+        printf("-----------------------------------------\n");
+    }
+    
+    
+
     return 0;
 }
