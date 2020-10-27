08c8ed6be4b7de97475fbbb4ed83237271c190dc
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2020-10-27 17:59:27 -0300

    teste

---
+++ ex006.c
@@ -0,0 +1,15 @@
+/*
+
+6. Construa um programa que permita armazenar o salário de 20 pessoas. Calcular e armazenar o novo salário sabendo-se que o reajuste foi de 8%. Imprimir uma listagem numerada com o salário e o novo salário. Declare quantos vetores forem necessários. 
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+    
+    return 0;
+}
